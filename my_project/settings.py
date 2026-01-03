5f199ca3d0c429dab1ed267337db2e15048dd9df
David Clamp <dclamp@yahoo.com>
2026-01-03 17:59:08 +0000

    Still project b

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u1_58*$2#(so3hkc068h9ba2ulw^i*wy5jxhme24s#*#bl7*7&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
